b885e7300e51cf34dae8e7ac6e67d6aa67aa13f7
brunomazzardo <bruno.mazzardo@hotmail.com>
2017-09-28 02:32:56 -0300

    Criando o algoritmo naive e seus samples

---
+++ naive.py
@@ -0,0 +1,14 @@
+text= "ababaaaa"
+pattern = "aaa"
+found=False
+for s in range ((len(text)-len(pattern))+1):
+    for j in range(len(pattern)):
+            if text[s+j] != pattern[j]:
+                break
+            elif j == len(pattern) - 1:
+                print 'Pattern occurs with shift' 
+                print s
+                found=True
+    
+if not found:
+    print 'No match found'    
